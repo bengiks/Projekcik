5ebe7820b2608c9b321c4adcee1e3903fed18584
Kacper Dluzen <kacperder@gmail.com>
2019-10-26 12:36:04 +0200

    plik z rejestracja (praca w toku)

---
+++ projekt/rejestracja.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+
+$host = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "bazadanych6";
+
+$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+
