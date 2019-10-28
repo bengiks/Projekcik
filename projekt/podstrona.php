9438c85a04462461a6542269144af0f57d57d533
MatiStopa <56273335+MatiStopa@users.noreply.github.com>
2019-10-28 16:42:14 +0100

    dodane pliki
    podstrona i css

--- projekt/podstrona.php
+++ projekt/podstrona.php
@@ -6,12 +6,18 @@
 <html>
 <head> 
     <meta charset="utf-8"/>
+    <link rel="stylesheet" href="css.css">
     <title>podstrona</title>
 </head>
 <body>
+<div class="stankonta">
 <?php
-    echo "<p>Zalogowano pomyślnie ".$_SESSION['uzytkownik']."!";
     echo "<p><b>Stan konta</b>: ".$_SESSION['stan konta']."PLN";
 ?>
+</div>
+<!-- <script type="application/javascript">
+    window.alert("<?php echo "Zalogowano pomyślnie ".$_SESSION['uzytkownik']."!"; ?>")
+</script> -->
+
 </body>
 </html>
